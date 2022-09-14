2b70049b8300fd988a3616b6c0a166d686a78c12
Juliana Andrade <juliana.andraden07@gmail.com>
2022-09-14 10:54:42 -0300

    Crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ItemToDo } from './ItemToDo';
+import { ListToDo } from './listToDo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tarefas';
+
+  public tarefa = "";
+  public tarefas  = ListToDo
+
+  addTarefa() {
+    let tarefaTeste: ItemToDo = {
+      id: '1',
+      descricao: this.tarefa,
+      dataCriacao: 0,
+      valorUpdate: ''
+    }
+    this.tarefas.push(tarefaTeste)
+    this.tarefa = ''
+  }
+
+  removeTarefa(item: ItemToDo) {
+    this.tarefas.splice(this.tarefas.indexOf(item), 1)
+  }
+
+  updateTarefa(item: ItemToDo) {
+    item.descricao = item.valorUpdate
+    item.valorUpdate = ''
+  }
 }
